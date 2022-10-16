a0c37cdfe3ebddfe58391827b2ab74c043f5884d
Mikhail Profatsky <mixail.rostov04@gmail.com>
2022-10-16 19:13:49 +0300

    added player and streaming response

--- app/routers/videos.py
+++ app/routers/videos.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, Form, UploadFile, File, Depends, HTTPException, status, BackgroundTasks
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, HTMLResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
 
 from app.schemas.auth import UserSchema
 from app.schemas.videos import VideoCreateSchema, VideoSchema
 from app.services.auth import get_current_user
 from app.services.videos import VideoService
 
+
+templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter(
     prefix="/videos",
@@ -28,13 +32,36 @@ async def upload_video(
     return await service.create(background_tasks, file, video_data)
 
 
-@router.get("/{video_id}", response_model=VideoSchema)
+@router.get("/{video_id}", response_class=HTMLResponse)
 async def get_video(
+        request: Request,
         video_id: int,
         service: VideoService = Depends()
 ):
     video = await service.get(video_id)
     if not video:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    file = open(video.file, mode="rb")
-    return StreamingResponse(file, media_type="video/mp4")
+    return templates.TemplateResponse(
+        "videos.html", {"request": request, "video_data": video}
+    )
+
+
+@router.get("/watch/{video_id}")
+async def get_streaming_video(
+        video_id: int,
+        request: Request,
+        service: VideoService = Depends()
+) -> StreamingResponse:
+    file, status_code, content_length, headers = await service.open_file(request, video_id)
+    response = StreamingResponse(
+        file,
+        media_type="video/mp4",
+        status_code=status_code
+    )
+
+    response.headers.update({
+        "Accept-Ranges": "bytes",
+        "Content-Length": str(content_length),
+        **headers
+    })
+    return response
--- app/services/videos.py
+++ app/services/videos.py
@@ -1,13 +1,16 @@
 import shutil
 from datetime import datetime
 from os import makedirs
+from pathlib import Path
 from uuid import uuid4
+from typing import IO, Generator
 
 from fastapi import Depends, UploadFile
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 from fastapi.background import BackgroundTasks
+from fastapi.requests import Request
 
 from app.database.database import get_session
 from app.models.videos import VideoModel
@@ -67,3 +70,48 @@ class VideoService:
         makedirs(file_path.rsplit("/", 1)[0], exist_ok=True)
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
+
+    async def open_file(self, request: Request, video_id: int) -> tuple | None:
+        video = await self._get(video_id)
+        path = Path(video.file)
+        file = path.open("rb")
+        content_length = file_size = path.stat().st_size
+
+        status_code = 200
+        headers = {}
+        content_range = request.headers.get("range")
+
+        if content_range is not None:
+            content_range = content_range.strip().lower()
+            content_ranges = content_range.split('=')[-1]
+            range_start, range_end, *_ = map(str.strip, (content_ranges + '-').split('-'))
+            range_start = max(0, int(range_start)) if range_start else 0
+            range_end = min(file_size - 1, int(range_end)) if range_end else file_size - 1
+            content_length = (range_end - range_start) + 1
+            file = self.ranged(file, start=range_start, end=range_end + 1)
+            status_code = 206
+            headers['Content-Range'] = f'bytes {range_start}-{range_end}/{file_size}'
+
+        return file, status_code, content_length, headers
+
+    @staticmethod
+    def ranged(
+            file: IO[bytes],
+            start: int = 0,
+            end: int = None,
+            block_size: int = 8192,
+    ) -> Generator[bytes, None, None]:
+        consumed = 0
+        file.seek(start)
+        while True:
+            data_length = min(block_size, end - start - consumed) if end else block_size
+            if data_length <= 0:
+                break
+            data = file.read(data_length)
+            if not data:
+                break
+            consumed += data_length
+            yield data
+
+        if hasattr(file, 'close'):
+            file.close()
